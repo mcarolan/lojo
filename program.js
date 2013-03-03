5cffd2e332e7ac45780b6cced2a519f6e25ad4f7
Martin Carolan <mcarolan@gmail.com>
2013-03-03 21:38:41 +0000

    500ms delay between running instructions allow user program to be external to html

--- program.js
+++ program.js
@@ -1,1 +1,16 @@
 fred = new turtle();
+
+function square() {
+	fred.move();
+	fred.face(north);
+	fred.move();
+	fred.face(west);
+	fred.move();
+	fred.face(south);
+	fred.move();
+	fred.face(east);	
+}
+
+square();
+fred.face(south);
+square();
